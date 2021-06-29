796a4e4914f9510371ace8d51bc9582b5f4f14c2
ashish140399 <ashish140399@gmail.com>
2021-06-29 17:12:32 +0530

    Resp changes

---
+++ src/components/Main/SortSelect.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Select from 'react-select'
+
+const SortSelect = ({className, handleSort, options, defaultValue, width}) => {
+    const customStyles = {
+        menu: (provided, state) => ({
+          ...provided,
+          width: state.selectProps.width,
+          background: '#0d1022',
+          color: state.selectProps.menuColor,
+          padding: 10,
+          zIndex: 100
+        }),
+        
+        control: (_, { selectProps: { width }}) => ({
+            width: width,
+            display: 'flex',
+            color: '#7f81a2',
+            borderBottom: '1px solid #45227e',
+            paddingLeft: '10px'
+        }),
+        
+        indicatorSeparator: (provided, state) => ({
+            display: 'none'
+        }),
+
+        option: (provided, state) => ({
+            ...provided,
+            color: '#7f81a2',
+            padding: 10,
+            background: '#0d1022',
+            '&:hover': {
+                background: '#0d1022'
+            }
+          }),
+
+        singleValue: (provided, state) => {
+            const opacity = state.isDisabled ? 0.5 : 1;
+            const transition = 'opacity 300ms';
+      
+            return { ...provided, 
+                opacity, 
+                transition,
+<<<<<<< HEAD
+                color: '#7f81a2'
+=======
+                color: 'white'
+>>>>>>> 811268a4f3535506abd90ef8dce2a980319143fe
+            };
+        }
+      }
+
+    return (
+        <Select
+            className={ className || "sort-select" }
+            defaultValue={options.length > defaultValue && options[defaultValue]}
+            isClearable={false}
+            isSearchable={false}
+            name="state"
+            styles={customStyles}
+            width={width || '200px'}
+<<<<<<< HEAD
+            menuColor='#7f81a2'
+=======
+            menuColor='white'
+>>>>>>> 811268a4f3535506abd90ef8dce2a980319143fe
+            options={options} 
+            onChange={handleSort}/>
+    )
+}
+
+export default SortSelect;
